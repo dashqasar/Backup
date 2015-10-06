9ef74f1f5c9ba590d7a7ac5e6fd97bda44baed3e
dashqasar <worrier318po@outlook.com>
2015-10-06 08:29:38 +0800

    first commit

---
+++ Assets/Script/CollideDetection.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollideDetection : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform  obj;
+	private Transform character;
+	private RaycastHit hit ;
+	private Renderer rend;
+	
+	private string SessionID = "";
+	
+	// Use this for initialization
+	void Awake () {
+		character = transform;
+		hit = new RaycastHit ();
+	}
+	void Start () {
+		rend = GetComponent<Renderer> ();
+		rend.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public bool CanSeeObject () {
+		Vector3 objDirection  = (obj.position - character.position);
+		
+		Physics.Raycast (character.position, objDirection,out hit, objDirection.magnitude);
+		if ( hit.collider && hit.collider.transform == obj ) {
+			return true;
+		}
+		return false;
+	}
+	
+	public void OnTriggerEnter (Collider other) {
+		if ( other.transform == obj && CanSeeObject() ) {
+			// do something;
+
+			rend.material.color = Color.red;
+			Debug.Log("I am collider");
+			//TrackCollision();
+		}
+	}
+}
---
+++ Assets/Script/UIController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+
+	// Use this for initialization
+	public Collider coll;
+	private bool isMove = false;
+	Vector3 origionPos;
+	private Renderer rend;
+	public GameObject ga ;
+	void Start() {
+		origionPos = transform.position;
+		rend = ga.GetComponent<Renderer> ();
+		rend.enabled = true;
+	}
+	void OnTriggerEnter(Collider other) {
+		isMove = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isMove == true) {
+			Vector3 pos = new Vector3(transform.position.x, transform.position.y - Time.deltaTime * 2, transform.position.z);
+			transform.position = pos;
+
+		}
+	
+	}
+	void OnGUI()
+	{
+		GUILayout.BeginArea(new Rect(Screen.width/2 -100, Screen.height -60, 400, 50));
+		
+		GUI.Label(new Rect (80, 20, 100, 20), "Please drage Mouse" );
+		
+		GUILayout.BeginHorizontal();
+
+		if ( GUI.Button(new Rect (15, 15, 60, 30), "Rest") ) 
+		{
+			isMove = false;
+			transform.position = origionPos;
+			rend.material.color = Color.white;
+
+		}
+
+		if ( GUI.Button(new Rect(165, 15, 60, 30), "Play") ) 
+		{
+			isMove = true;
+
+		}
+
+		GUILayout.EndArea ();
+	}
+}
